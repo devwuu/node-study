4157dd8b3bd8b3157c9660bc308741f9fc13dc93
devwuu <jdw0477@gmail.com>
2023-09-25 16:42:13 +0900

    DB 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module'; // 각 도메인 별로 모듈을 따로 만든다.
 import { LoggerMiddleware } from './common/middlewares/logger/logger.middleware';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import * as process from "process";
 
 // Nestjss는 모든 파일이 거의 모듈화가 되어 있다. 따라서 각 모듈을 등록해서 사용하게 돼있음
 // 의존성 주입이 돼야 하는 것들이 등록되는 부분이 providers 이다
@@ -10,8 +13,13 @@ import { LoggerMiddleware } from './common/middlewares/logger/logger.middleware'
 // import ?
 // (1) 다양한 모듈을 app 모듈에서 합쳐서 하나의 애플리케이션으로 서비스 된다
 // (2) exports 된 다른 모듈을 현재 모듈에서 사용할 수 있다 ( 이렇게 해야 남의 service 등을 주입받을 수 있다)
+
 @Module({
-  imports: [CatsModule],
+  imports: [
+    CatsModule,
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+  ],
   controllers: [AppController],
   providers: [AppService],
   // 자기 모듈이 아닌 곳에 있는 service는 Provider에 바로 때려박는 게 아님
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
+import * as process from "process";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
 }
 bootstrap();

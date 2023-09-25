b81fe2abda829b51985105810379c998d9dce6cb
devwuu <jdw0477@gmail.com>
2023-09-25 15:16:29 +0900

    사용자정의 pipe

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -13,6 +13,7 @@ import {
 } from "@nestjs/common";
 import { CatsService } from './cats.service';
 import { HttpExceptionFilter } from "../http-exception.filter";
+import { PositiveIntPipe } from "../common/positiveInt.pipe";
 
 @Controller('cats')
 export class CatsController {
@@ -24,13 +25,19 @@ export class CatsController {
   }
   @Get('/:id')
   // @UseFilters(HttpExceptionFilter) // 필터는 특정 메서드, 특정 컨트롤러에만 적용할 수도 있다
-  findById(@Param('id', ParseIntPipe) id: number){
+  findById(@Param('id', ParseIntPipe, PositiveIntPipe) id: number){
 
     // Pipe는 자동으로 형변환 및 validation을 해줍니다
     console.log(typeof id);
+    // Pipe는 여러개를 등록해서 사용 할 수 있습니다
+    // 여러개를 사용할 경우 앞에서부터 순차적으로 실행됩니다 ( pipe-filter pattern)
+    // 만약 중간에 validation error가 발생한다면 exception을 일으키며 로직을 탈출함
 
     // throw new HttpException('API IS BROKEN', HttpStatus.FORBIDDEN);
     // 에러 처리를 할 때 보통 이 에렇게 많이 사용합니다. 여기서 status는 http code
+
+    // 필터, 파이프랑 관련된 라이프 사이클
+    //https://docs.nestjs.com/faq/request-lifecycle#summary
     return 'find By id';
   }
   @Post()

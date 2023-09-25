48883c4d4a01b1f0c29f37af38ca839bb4484bcf
devwuu <jdw0477@gmail.com>
2023-09-25 14:59:05 +0900

    exception의 global 처리

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,15 +1,32 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Put } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  Put,
+  UseFilters
+} from "@nestjs/common";
 import { CatsService } from './cats.service';
+import { HttpExceptionFilter } from "../http-exception.filter";
 
 @Controller('cats')
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
   @Get()
   findAll(){
+    throw new HttpException('API IS BROKEN', HttpStatus.FORBIDDEN);
     return 'find all';
   }
   @Get('/:id')
+  @UseFilters(HttpExceptionFilter) // 필터는 특정 메서드에만 적용할 수 있다
   findById(@Param('id') id: string){
+    throw new HttpException('API IS BROKEN', HttpStatus.FORBIDDEN);
+    // 에러 처리를 할 때 보통 이 에렇게 많이 사용합니다. 여기서 status는 http code
     return 'find By id';
   }
   @Post()

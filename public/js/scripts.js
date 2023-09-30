891393bf315394f397969d77b408e41d15a9eda4
devwuu <jdw0477@gmail.com>
2023-09-30 17:45:53 +0900

    emit, on 기본

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -6,12 +6,17 @@ const chattingBoxElement = getElementById('chatting_box');
 const formElement = getElementById('chat_form');
 
 function init() {
-  let username = helloUser();
-  console.log(`username is... ${username}`);
+  helloUser();
 }
 
 function helloUser() {
-  return prompt('What is ur name?');
+  let username = prompt('What is ur name?');
+  socket.emit('new_user', username, (data) => {
+    console.log(`server return is... ${data}`); // server에서 return 한 메세지를 받을 수 있다
+  }); // 이벤트 발생 시켜서 server로 데이터 전송
+  socket.on('hello_user', (data) => {
+    console.log(data);
+  }); // 서버에서 발생시킨 hello_user 이벤트를 catch 할 수 있다
 }
 
 init();
--- src/chats/chats.gateway.ts
+++ src/chats/chats.gateway.ts
@@ -1,9 +1,22 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import {
+  ConnectedSocket,
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+} from '@nestjs/websockets';
 
 @WebSocketGateway()
 export class ChatsGateway {
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
+  // client에서 발생 시킨 event를 catch 한다 ('on'이라고 생각하면 됨) ==> socket.on('new_user', ...)
+  // client에서 emit 시킨 event 이름과 SubscribeMessage의 인자를 일치 시켜준다
+  @SubscribeMessage('new_user')
+  findNewUser(
+    @MessageBody() username: string, // client에서 보낸 메세지.
+    @ConnectedSocket() socket, // 연결된 소켓. 연결이 끊기기 전까지는 동일한 id로 연결이 유지된다. 그래서 연결된 user를 식별할 수 있다
+  ): string {
+    console.log(`username is... ${username}`);
+    console.log(`socket id is... ${socket.id}`);
+    socket.emit('hello_user', `hello, ${username}`); // client로 데이터 전송
+    return 'hello anonymous user'; // client에서 new_user를 emit 시킨 함수의 callback 함수로 받을 수 있다
   }
 }

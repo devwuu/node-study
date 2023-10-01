b25121f2e94c910d049a51a805b710297c2c601e
devwuu <jdw0477@gmail.com>
2023-10-01 17:09:29 +0900

    socket create dto 수정

--- src/chats/dtos/sockets.create.dto.ts
+++ src/chats/dtos/sockets.create.dto.ts
@@ -2,7 +2,6 @@ import { PickType } from '@nestjs/mapped-types';
 import { Sockets } from '../models/sockets.model';
 
 export class SocketsCreateDto extends PickType(Sockets, [
-  '_id',
   'id',
   'username',
 ] as const) {}

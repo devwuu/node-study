b9f8bcff16e314dc75585e565b1a706a394bd522
devwuu <jdw0477@gmail.com>
2023-10-01 12:45:00 +0900

    gateway namespace gateway lifecycle

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,4 +1,7 @@
-const socket = io('/');
+const socket = io('/chatting'); // 파라미터로 네임스페이스를 넣어준다
+// socket io 라이브러리는 Polling 방식으로 1차 시도를 해보고
+// websocket을 지원하는 브라우저면 websocket으로 통신한다
+// 즉, socket io 라이브러리는 브라우저 환경에 따라 실시간 통신을 할 수 있게 해주는 라이브러리
 const getElementById = (id) => document.getElementById(id) || null;
 
 const helloStrangerElement = getElementById('hello_stranger');
--- src/chats/chats.gateway.ts
+++ src/chats/chats.gateway.ts
@@ -1,12 +1,20 @@
 import {
   ConnectedSocket,
   MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
   SubscribeMessage,
   WebSocketGateway,
 } from '@nestjs/websockets';
+import { Logger } from '@nestjs/common';
 
-@WebSocketGateway()
-export class ChatsGateway {
+@WebSocketGateway({ namespace: 'chatting' }) // namespace 를 옵션으로 설정해줄 수 있음
+export class ChatsGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  private logger = new Logger();
+
   // client에서 발생 시킨 event를 catch 한다 ('on'이라고 생각하면 됨) ==> socket.on('new_user', ...)
   // client에서 emit 시킨 event 이름과 SubscribeMessage의 인자를 일치 시켜준다
   @SubscribeMessage('new_user')
@@ -18,5 +26,25 @@ export class ChatsGateway {
     console.log(`socket id is... ${socket.id}`);
     socket.emit('hello_user', `hello, ${username}`); // client로 데이터 전송
     return 'hello anonymous user'; // client에서 new_user를 emit 시킨 함수의 callback 함수로 받을 수 있다
+  }
+
+  // gateway life cycle
+  constructor() {
+    this.logger.log('constructor...');
+  }
+
+  // OnGatewayInit 인터페이스 구현
+  afterInit(server: any): any {
+    this.logger.log('after init...');
+  }
+
+  //OnGatewayConnection 인터페이스 구현
+  handleConnection(@ConnectedSocket() socket): any {
+    this.logger.log(`connected...${socket.id} ${socket.nsp.name}`); // client와 연결이 된 이후에 실행됨.
+  }
+
+  //OnGatewayDisconnect 인터페이스 구현
+  handleDisconnect(@ConnectedSocket() socket): any {
+    this.logger.log(`disconnected...${socket.id} ${socket.nsp.name}`); // client와 연결이 끊어진 이후에 실행됨.
   }
 }
